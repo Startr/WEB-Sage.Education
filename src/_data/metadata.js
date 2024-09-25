8d95351f75fbdbd0b0c828c2bbd7e622a795a50a
OpenCo <admin@openco.ca>
2024-09-25 11:51:53 +0000

    Add metadata.js file with site information

---
+++ src/_data/metadata.js
@@ -0,0 +1,12 @@
+module.exports = {
+	title: "Sage Education",
+	url: "https://sage.education",
+	language: "en",
+	name: "Sage Education",
+	description: "Sage Education for all your educational needs",
+	author: {
+		name: "",
+		email: "youremailaddress@example.com",
+		url: "https://example.com/about-me/"
+	}
+}

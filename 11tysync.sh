28754eebd3f4aafdac4b6304c85bd5ffd15b4ca9
OpenCo <admin@openco.ca>
2024-10-17 16:02:32 +0000

    feat: adding 11tysync.sh script

---
+++ 11tysync.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# This should be able to be run as a Platypus script but we need to make sure that the PATH is set correctly
+# fswatch is installed via Homebrew and is located in /usr/local/bin
+
+#TODO solve the platypus issue with the PATH
+
+echo "STATUSTITLE|🔄 11ty 🔄"
+
+# Define source and destination directories
+SRC_DIR="/Users/somma/Documents/Projects/Family Thoughts/Family Thoughts/Projects/Sites/Sage.Education"
+DEST_DIR="/Users/somma/Documents/Projects/GitHub/WEB-Sage.Education/src/sage.education"
+
+# Function to sync directories using rsync
+sync_directories() {
+    rsync -avz --delete "$SRC_DIR/" "$DEST_DIR/"
+}
+
+
+# Initial sync
+sync_directories
+
+# Monitor changes in source directory using fswatch
+fswatch -0 -r "$SRC_DIR" | while read -d "" event; do
+    echo "Change detected: $event"
+    sync_directories
+done

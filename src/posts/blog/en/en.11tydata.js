113195742bbfd972f40f32f622bfd1ce47b9c0ad
Alexander Somma <admin@openco.ca>
2026-04-01 14:15:12 +0000

    Clean up directory data: remove redundant tags
    Collection uses glob, not tags. The layout: field stays to provide
    layouts/blog.njk for all articles in this directory.

--- src/posts/blog/en/en.11tydata.js
+++ src/posts/blog/en/en.11tydata.js
@@ -1,6 +1,5 @@
 module.exports = {
   layout: "layouts/blog.njk",
-  tags: ["post"],
   eleventyComputed: {
     description: (data) => data.description || data.summary || data.subheadline || "",
   }
